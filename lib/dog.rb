db5c8e5de6641c823eb3b37a9880f596091c3037
Sam Ferebee <sam.ferebee@gmail.com>
2021-01-14 10:49:23 -0500

    commit tree f6c732ed2db0d4b783a833ad84c1a3310657f5ff

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
-# Add your code here
+class Dog
+
+    @@all = []
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        self.all.each {|dog| puts dog.name}
+    end
+
+    def self.clear_all
+        self.all.clear
+    end
+
+    def save
+        @@all.push(self)
+    end
+
+end
